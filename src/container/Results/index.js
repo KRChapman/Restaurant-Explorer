7433169452801362354ad94721bd4822f2ca9cd6
Kyle Chapman <splash004@gmail.com>
2019-09-08 15:19:03 -0700

    first commit

---
+++ src/container/Results/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+  // use hoc
+class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  )
+  }
+}       
+ 
+export default Results;
